35fcdc75d00a1112166f52054556fa58daa6db88
Alberto <alberto.cst05@gmail.com>
2024-09-23 20:42:34 +0200

    probando_rotacion

--- Assets/my_Folder/Scripts/Player_Controller.cs
+++ Assets/my_Folder/Scripts/Player_Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
@@ -10,15 +11,23 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] private float rotationPlayerSpeed;
     private float horizontalInput;
      private float verticalInput;
+    private bool isGameOver;
+
+    // prueba de game over
+
+    [SerializeField] private GameObject gameOverPanel;
 
     private void Start()
     {
-        
+        gameOverPanel.SetActive(false);
     }
     private void Update()
     {
         PlayerMovment();
         PlayerRotation();
+        GameOver();
+        Debug.Log(isGameOver);
+
     }
     
     private void PlayerMovment()
@@ -36,5 +45,16 @@ public class Player_Controller : MonoBehaviour
         
             transform.Rotate(Vector3.up * rotationPlayerSpeed * horizontalInput * Time.deltaTime);
         
+    }
+
+    private void GameOver()
+    {
+        if (transform.rotation.y <= -90f || transform.rotation.y >= 90f )
+        {
+            Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+            isGameOver = true;
+
+        }
     }
 }
