790535b5cee699e0b3a4bbf22c2239d5b5d96271
albertoCC05 <alberto.cst05@gmail.com>
2024-09-23 13:51:11 +0200

    player movment ready

--- Assets/my_Folder/Scripts/Player_Controller.cs
+++ Assets/my_Folder/Scripts/Player_Controller.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //movment variables
+
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float rotationPlayerSpeed;
+    private float horizontalInput;
+     private float verticalInput;
+
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        PlayerMovment();
+        PlayerRotation();
+    }
+    
+    private void PlayerMovment()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        
+        if (verticalInput >= 0 )
+        {
+            transform.Translate(Vector3.forward * playerSpeed * verticalInput * Time.deltaTime);
+        }
+    }
+    private void PlayerRotation()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+            transform.Rotate(Vector3.up * rotationPlayerSpeed * horizontalInput * Time.deltaTime);
         
     }
 }
