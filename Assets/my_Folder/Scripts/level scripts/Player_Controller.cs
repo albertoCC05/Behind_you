12daf0346cd4a5d1804e7758501999be2a615b87
albertoCC05 <alberto.cst05@gmail.com>
2024-09-24 12:17:10 +0200

    rotacion_parte1_hecho

--- Assets/my_Folder/Scripts/level scripts/Player_Controller.cs
+++ Assets/my_Folder/Scripts/level scripts/Player_Controller.cs
@@ -10,11 +10,16 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] private float playerSpeed;
     [SerializeField] private float rotationPlayerSpeed;
     private float horizontalInput;
-     private float verticalInput;
+    private float verticalInput;
     private bool isGameOver;
+    
+
+
 
     // prueba de game over
 
+    private float losePositiveRotations = 0.90f;
+    private float loseNegativeRotations = -0.90f;
     [SerializeField] private GameObject gameOverPanel;
 
     private void Start()
@@ -26,10 +31,14 @@ public class Player_Controller : MonoBehaviour
         PlayerMovment();
         PlayerRotation();
         GameOver();
-        Debug.Log(isGameOver);
+
+        
+        
 
     }
-    
+
+   
+
     private void PlayerMovment()
     {
         verticalInput = Input.GetAxis("Vertical");
@@ -39,6 +48,7 @@ public class Player_Controller : MonoBehaviour
             transform.Translate(Vector3.forward * playerSpeed * verticalInput * Time.deltaTime);
         }
     }
+   
     private void PlayerRotation()
     {
         horizontalInput = Input.GetAxis("Horizontal");
@@ -47,9 +57,15 @@ public class Player_Controller : MonoBehaviour
         
     }
 
+ 
+   
+    
+
     private void GameOver()
     {
-        if (transform.rotation.y <= -90f || transform.rotation.y >= 90f )
+        
+
+        if (transform.rotation.y <= loseNegativeRotations || transform.rotation.y >= losePositiveRotations )
         {
             Time.timeScale = 0;
             gameOverPanel.SetActive(true);
---
+++ Assets/my_Folder/Scripts/level scripts/TrigerPoint.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrigerPoint : MonoBehaviour
+{
+    [SerializeField] private float newRotationPositive;
+    [SerializeField] private float newRotationNegative;
+}
